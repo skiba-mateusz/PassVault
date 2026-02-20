d597da669c85c0b5455b50e4a0bc78062fdc6947
Mateusz <mateusz.skiba114@gmail.com>
2026-02-20 18:18:36 +0100

    feat: add basic encryption functions

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/skiba-mateusz/PassVault
+
+go 1.25.0
+
+require (
+	golang.org/x/crypto v0.48.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"log"
+
+	"golang.org/x/crypto/argon2"
+)
+
+func main() {
+	fmt.Println("PassVault")
+
+	password := "password"
+	salt := "salt"
+	key := derieveKey([]byte(password), []byte(salt))
+	fmt.Println(key)
+
+	plaintext := "secret"
+
+	ciphertext, err := encrypt([]byte(plaintext), key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	fmt.Println(ciphertext)
+
+	decrypted, err := decrypt(ciphertext, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(decrypted)
+}
+
+func derieveKey(password, salt []byte) []byte {
+	return argon2.IDKey(password, salt, 1, 32*64, 4, 32)
+}
+
+func encrypt(plaintext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+
+	return ciphertext, nil
+}
+
+func decrypt(ciphertext, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := ciphertext[:gcm.NonceSize()]
+	ciphertext = ciphertext[gcm.NonceSize():]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
